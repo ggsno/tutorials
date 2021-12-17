64189597e4c6008dd2d88bb992fbc08ac4a6a0b8
ks <ogs0518@gmail.com>
2021-12-17 19:02:56 +0900

    update routers

--- nomadcoder/GraphQL/apollo/src/routes/Detail.js
+++ nomadcoder/GraphQL/apollo/src/routes/Detail.js
@@ -1,1 +1,29 @@
-export default () => "Detail";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import Movie from "../components/Movie";
+import { useParams } from "react-router-dom";
+
+const Detail = () => {
+  const { id } = useParams();
+  const GET_BYID = gql`
+{
+  getById(id:${id}){
+    id
+    title
+    background_image
+  }
+}
+`;
+  const { loading, error, data } = useQuery(GET_BYID);
+  if (loading) return "Loading ...";
+  console.log(data.getById.background_image);
+  if (data?.getById)
+    return (
+      <div>
+        <div>{data.getById.title}</div>
+        <img src={data.getById.background_image} alt="ppap" />
+        </div>
+    );
+};
+
+export default Detail;
--- nomadcoder/GraphQL/apollo/src/routes/Home.js
+++ nomadcoder/GraphQL/apollo/src/routes/Home.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client"
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
-    movies{
-      id,
+    getMovies{
+      id
       medium_cover_image
     }
   }
 `;
-
-export default () => {
+const Home = () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
   if (loading) return "Loading ...";
-  if (data && data.movies) return data.movies.map(movie => <h1>{movie.id}</h1>);
+  if (data?.getMovies) {
+    return (
+      data.getMovies.map(movie => <Movie key={movie.id} id={movie.id}/>)
+      );
+    }
 };
+
+
+export default Home;
